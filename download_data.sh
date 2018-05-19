74cc7c3d6befbda85672c824134516bf19a187bc
Bradley Bossard <bradleybossard@gmail.com>
2018-05-19 11:52:50 -0700

    Basic script for downloading data

---
+++ download_data.sh
@@ -0,0 +1,8 @@
+# TODO: Rewrite as loop and check for existance of file before downloading
+curl https://datasets.imdbws.com/name.basics.tsv.gz --output name.basics.tsv.gz
+curl https://datasets.imdbws.com/title.akas.tsv.gz --output title.akas.tsv.gz
+curl https://datasets.imdbws.com/title.basics.tsv.gz --output title.basics.tsv.gz
+curl https://datasets.imdbws.com/title.crew.tsv.gz --output title.crew.tsv.gz
+curl https://datasets.imdbws.com/title.episode.tsv.gz --output title.episode.tsv.gz
+curl https://datasets.imdbws.com/title.principals.tsv.gz --output title.principals.tsv.gz
+curl https://datasets.imdbws.com/title.ratings.tsv.gz --output title.ratings.tsv.gz
